2f4fc7ddd9f5464b0359eed444c9114730e5f6dc
Ubuntu <arojaspa@UBU-ING-DATOS.ptjctmwzoiqerk1qovd2shjs1b.cx.internal.cloudapp.net>
2025-11-18 03:27:07 +0000

    content updated after the session 2

--- session_2/1_testpy.py
+++ session_2/1_testpy.py
@@ -1,3 +1,7 @@
 import pandas as pd
+import numpy as np
+import matplotlib as mp
 
-print(pd.__version__)
+print(f'Version de pandas: {pd.__version__}')
+print(f'Version de numpy: {np.__version__}')
+print(f'Version de matplotlib: {mp.__version__}')
