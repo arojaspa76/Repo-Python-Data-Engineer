d795978e5ec4341fe6841e3b738873b6bc24a147
Andres Rojas <arojaspa@outlook.com>
2025-12-17 19:22:39 -0500

    repository update

---
+++ session_9/glue_pipeline/sales_etl_job.py
@@ -0,0 +1,21 @@
+import sys
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+
+# Obtener parámetros
+args = getResolvedOptions(sys.argv, ['JOB_NAME'])
+
+# Inicializar contextos
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+job.init(args['JOB_NAME'], args)
+
+# Tu código ETL aquí...
+print("Job iniciado correctamente")
+
+job.commit()
